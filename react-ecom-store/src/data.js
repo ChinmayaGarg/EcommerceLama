ff554980d653c5b1f4ff244f39b1878421b34a56
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-08-07 00:45:28 +0530

    Slider / Carousel Added

---
+++ react-ecom-store/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: require('./images/home/img1.png'),
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'F5FAFD',
+  },
+  {
+    id: 2,
+    img: require('./images/home/img1.png'),
+    title: 'AUTUMN COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: require('./images/home/img1.png'),
+    title: 'POPULAR COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fbf0f4',
+  },
+];
--- react-ecom-store/src/pages/Home.jsx
+++ react-ecom-store/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
+import Slider from '../components/Slider';
 
 const Home = () => {
   return (
     <div>
-      <Announcement /> <Navbar />;
+      <Announcement /> <Navbar /> <Slider />
     </div>
   );
 };
