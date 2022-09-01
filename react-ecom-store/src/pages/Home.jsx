a0373eec588ded7c0594798fe4cbde90827f769f
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-09-02 02:20:53 +0530

    Newsletter Component Added

--- react-ecom-store/src/pages/Home.jsx
+++ react-ecom-store/src/pages/Home.jsx
@@ -4,11 +4,12 @@ import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
 import Categories from '../components/Categories';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
   return (
     <div>
-      <Announcement /> <Navbar /> <Slider /> <Categories /> <Products />
+      <Announcement /> <Navbar /> <Slider /> <Categories /> <Products /> <Newsletter />
     </div>
   );
 };
