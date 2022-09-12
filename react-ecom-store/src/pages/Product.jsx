1de3e0c6a59a33df590cbe00eb4147151ac646bd
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-09-12 21:53:42 +0530

    Product Page Made Responsive

--- react-ecom-store/src/pages/Product.jsx
+++ react-ecom-store/src/pages/Product.jsx
@@ -5,12 +5,19 @@ import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { mobile } from '../responsive';
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${mobile({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '10px',
+    // margin: '0px 20px',
+  })}
 `;
 
 const ImgContainer = styled.div`
@@ -21,11 +28,18 @@ const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
+  ${mobile({
+    height: '40vh',
+  })}
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
-  padding 0px 50px `;
+  padding: 0px 50px;
+  ${mobile({
+    padding: '10px',
+  })}
+`;
 
 const Title = styled.h1`
   font-weight: 200;
@@ -45,6 +59,9 @@ const FilterContainer = styled.div`
   justify-content: space-between;
   width: 50%;
   margin: 30px 0px;
+  ${mobile({
+    width: '100%',
+  })}
 `;
 
 const Filter = styled.div`
@@ -78,6 +95,9 @@ const AddContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 50%;
+  ${mobile({
+    width: '100%',
+  })}
 `;
 
 const AmountContainer = styled.div`
