ab18e07fee5acb1dd28089ff0f1c6585ad241993
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-09-12 21:45:06 +0530

    DeviceHelper Integrated

---
+++ react-ecom-store/src/DeviceHelper.js
@@ -0,0 +1,34 @@
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+export const isWindows = () => {
+  if (/windows phone/i.test(userAgent)) {
+    return true;
+  }
+};
+export const isAndroid = () => {
+  if (/android/i.test(userAgent)) {
+    return true;
+  }
+};
+export const isIos = () => {
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return true;
+  }
+};
+
+export const DeviceHelper = () => {
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return 'Windows Phone';
+  }
+
+  if (/android/i.test(userAgent)) {
+    return 'Android';
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'iOS';
+  }
+  return 'unknown';
+};
