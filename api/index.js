fb953d8bd5c8a32e30ac5cadbcc4fc397ee8478f
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-09-15 00:21:59 +0530

    Step 7.5.1: Using connection string in code to connect DB with Backend

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://admin:<password>@cluster0.5qikr1n.mongodb.net/?retryWrites=true&w=majority');
+
 app.listen(5000, () => {
   console.log('Backend server is running. This callback function will run after starting the server');
 });
