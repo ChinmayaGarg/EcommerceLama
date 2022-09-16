22fe7407a34825737d417387576962c749e8cdd2
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-09-16 22:40:23 +0530

    Auth API (Registeration Part) Created for User

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.get('/api/test', () => {
 app.use(express.json()); // Middleware integrated: This will allow accepting JSON object from client request
 
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running. This callback function will run after starting the server');
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
+const User = require('../models/User');
+
+// REGISTER
+
+/* 
+Made the callback function async because .save() is async function and hence without saving the new user in 
+the collection it will run the next line. So to make the process synchronous we have used async and await.
+
+We have put .save() inside try-catch block to cathc any error if occurs while saving the new user in the 
+collection
+
+201 status stands for Created.
+*/
+router.post('/register', async (req, res) => {
+  try {
+    if (!!!req.body.username) throw 'Username not entered';
+    if (!!!req.body.email) throw 'Email not entered';
+    if (!!!req.body.password) throw 'Password not entered';
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
